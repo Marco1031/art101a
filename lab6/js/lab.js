a18420ba8797ba70c9eaaf9a40b1f96a987a8903
Marco1031 <bernalbest03@gmail.com>
2024-04-28 22:33:09 -0700

    lab  6 updates

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,11 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - experiment and use arrays and object
+// Author: Marco Bernal
+// Date: April 28, 2024
 
 // Constants
-
+myTransport = ["driving","walking", "bus", "rides from friends"];
+myMainRide = {
+  make: "Volvo",
+  model: "c70 t5",
+  color: "black",
+  year: 2007,
+  age: function(){
+    return 2024 - this.year;
+  }
+}
 // Functions
-
+myTransport.unshift(myMainRide.year + " " + myMainRide.make + " " + myMainRide.model);
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
@@ -13,7 +22,9 @@ function myFunction(param1, param2) {
 }
 
 function main() {
-  console.log("Main function started.");
+  document.writeln("kinds of transportation that I use: " + myTransport.join(", "), "<br>" );
+  //delete myMainRide.fullkit;
+  document.writeln("My Main Ride: <pre>" + JSON.stringify(myMainRide, null, "\t", "</pre>)"));
   // the code that makes everything happen
 }
 
