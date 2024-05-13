ffdf0e84e36ee0731987947c03ad593cbe2981e9
Marco1031 <bernalbest03@gmail.com>
2024-05-12 23:22:06 -0700

    lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,15 @@
+/*
+   lab.js - This script adds buttons to different sections of the webpage and toggles a class on click
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Marco Bernal
+   Date: may 12 2024
+*/
+
+["challenge", "problems", "results"].forEach(section => {   //checks each section
+    $(`#${section}`).append(`<button id='button-${section}'>Toggle ${section}</button>`) //adds a button to each object
+        .click(function() {     //event listner for click
+            $(`#${section}`).toggleClass("special").siblings().removeClass("special"); //toggles the class addition/removal
+        });
+  });
